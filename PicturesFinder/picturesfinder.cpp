d6a49d7186461fa87df99fa5140817b821265b83
Siarhei Trubchyk <sergeitrubchik95@gmail.com>
2018-05-03 20:06:46 +0300

    Clean up.

--- PicturesFinder/picturesfinder.cpp
+++ PicturesFinder/picturesfinder.cpp
@@ -18,6 +18,7 @@ PicturesFinder::~PicturesFinder()
 
 void PicturesFinder::on_browseButton_clicked()
 {
+    // open file dialog and get path to directory
     QString str = QFileDialog::getExistingDirectory(0, "Directory Dialog", "");
     ui->lineEdit->setText(str);
 }
@@ -29,24 +30,51 @@ void PicturesFinder::on_startButton_clicked()
     if(!dir.exists())
     {
         QMessageBox mb;
-        mb.setText("Directory '" + directoryName + "' doesn't exist. Check path.");
+        mb.setText("Directory '" + directoryName + "' doesn't exist. Please check path's correctness.");
         mb.exec();
+        return;
     }
 
-    QVBoxLayout* flowLayout = new QVBoxLayout;
+    QVBoxLayout* layout = new QVBoxLayout;
     QWidget* scrollAreaContent = new QWidget;
-    scrollAreaContent->setLayout( flowLayout );
-    ui->scrollArea->setHorizontalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
-    ui->scrollArea->setVerticalScrollBarPolicy( Qt::ScrollBarAsNeeded );
-    ui->scrollArea->setWidgetResizable( true );
-    ui->scrollArea->setWidget( scrollAreaContent );
+    scrollAreaContent->setLayout(layout);
+
+    ui->scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    ui->scrollArea->setWidgetResizable(true);
+    ui->scrollArea->setWidget(scrollAreaContent);
 
     QDirIterator* dirIter = new QDirIterator(directoryName, QStringList() << "*.jpg" << "*.JPG", QDir::Files,QDirIterator::Subdirectories);
+    // iterate over all '.jpg' files
     while(dirIter->hasNext())
     {
-        auto next = dirIter->next();
-        QLabel *imageLabel = new QLabel;
-        imageLabel->setPixmap(QPixmap::fromImage(QImage(next)));
-        flowLayout->addWidget(imageLabel);
+        // get full path to image
+        auto filename = dirIter->next();
+        if (!filename.isEmpty())
+        {
+            // try to load image
+            QImage image(filename);
+            if (!image.isNull())
+            {
+                // create pixmap
+                QPixmap pixmap = QPixmap::fromImage(image);
+
+                // create label and set pixmap
+                QLabel *imageLabel = new QLabel;
+                imageLabel->setPixmap(pixmap);
+                imageLabel->setScaledContents(true);
+                imageLabel->setSizePolicy(QSizePolicy::Maximum,QSizePolicy::Maximum);
+
+                // add this label widget to layout
+                layout->addWidget(imageLabel);
+            }
+            //TODO: add logs window
+            //else
+            //{
+
+            //    QMessageBox::information(this, tr("PicturesFinder"),
+            //                             tr("Cannot load %1.").arg(filename));
+            //}
+        }
     }
 }
