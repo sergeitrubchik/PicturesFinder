3b3a9316578d6506301d5a7a4122e3db359b1bd8
Siarhei Trubchyk <sergeitrubchik95@gmail.com>
2018-05-04 00:15:27 +0300

    Extend implementation of LoadThread.

--- PicturesFinder/loadthread.cpp
+++ PicturesFinder/loadthread.cpp
@@ -1,6 +1,51 @@
 #include "loadthread.h"
+#include <QLabel>
+#include <iostream>
+#include <fstream>
 
-LoadThread::LoadThread()
+LoadThread::LoadThread(QMutex* mutex, QDirIterator* dirIter, QList<QPixmap*>& pixmaps, QString name)
+    :QRunnable(),
+      mutex_(mutex),
+      dirIter_(dirIter),
+      pixmaps_(pixmaps),
+      name_(name)
 {
 
 }
+
+void LoadThread::run()
+{
+    mutex_->lock();
+    bool hasNext = dirIter_->hasNext();
+
+    // iterate over all '.jpg' files
+    while(hasNext)
+    {
+        // get full path to image
+        auto filename = dirIter_->next();
+        qDebug() << filename << name_;
+        mutex_->unlock();
+
+        if (!filename.isEmpty())
+        {
+            // try to load image
+            QImage image(filename);
+            if (!image.isNull())
+            {
+                // create pixmap and add it to list
+                QPixmap* pixmap = new QPixmap(QPixmap::fromImage(image));
+                mutex_->lock();
+                pixmaps_.append(pixmap);
+                mutex_->unlock();
+            }
+            //TODO: add logs window "Cannot load %1.").arg(filename));
+        }
+
+        mutex_->lock();
+        hasNext = dirIter_->hasNext();
+    }
+
+    mutex_->unlock();
+    qDebug() << name_ << " done.";
+}
+
--- PicturesFinder/loadthread.h
+++ PicturesFinder/loadthread.h
@@ -1,11 +1,23 @@
 #ifndef LOADTHREAD_H
 #define LOADTHREAD_H
 
+#include <QtCore>
+#include <QLayout>
 
-class LoadThread
+class LoadThread : public QRunnable
 {
+    // TODO: what are main differences between QThread and QRunnable?
 public:
-    LoadThread();
+    LoadThread(QMutex* mutex, QDirIterator* dirIter, QList<QPixmap*>& pixmaps, QString name);
+
+protected:
+    void run();
+
+private:
+    QMutex* mutex_;
+    QDirIterator* dirIter_;
+    QList<QPixmap*>& pixmaps_;
+    QString name_;
 };
 
 #endif // LOADTHREAD_H
